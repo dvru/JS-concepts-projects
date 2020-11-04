7499fa67fff6babe0c943557e16ece6efaab5d2d
dvru <damvaru4@gmail.com>
2020-11-03 21:44:59 -0600

    added user button functionality

--- P&P/script.js
+++ P&P/script.js
@@ -1,1 +1,18 @@
-console.log('testing');
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt user to select media, pass to video element, then play
+async function selectMedia() {
+    try {
+        const media = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = media;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch (error) {
+        console.log('whoops, error here:', error)
+    }
+}
+
+// on Load
+selectMedia();
